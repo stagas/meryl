4958b7075939792a4be83a666ac140e87b26ac73
kadir pekel <kadir@li19-246.members.linode.com>
2010-07-09 15:17:46 +0000

    initial check in

---
+++ examples/hello.js
@@ -0,0 +1,15 @@
+require.paths.unshift('../lib');
+
+var sys = require('sys'),
+	http = require('http'),
+	meryl = require('meryl');
+
+meryl.h('GET /hello_world', function(ctx) {
+	ctx.resp.writeHead(200, {'Content-Type': 'text/html'});
+	ctx.resp.write('<h1>Hello, World!</h1>');
+	ctx.resp.write('<pre> url parts:' + JSON.stringify(ctx.parts) + '</pre>');
+	ctx.resp.end();
+});
+
+http.createServer(meryl.cgi).listen(8080);
+
