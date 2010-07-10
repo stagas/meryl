848acba003c5f58a22fb8223e3eb44ba955d8812
Kadir Pekel <kadirpekel@gmail.com>
2010-07-11 02:59:42 +0300

    some minor cleanup

--- examples/hello.js
+++ examples/hello.js
@@ -7,7 +7,6 @@ var sys = require('sys'),
 meryl.h('GET /hello_world', function(ctx) {
 	ctx.resp.writeHead(200, {'Content-Type': 'text/html'});
 	ctx.resp.write('<h1>Hello, World!</h1>');
-	ctx.resp.write('<pre> url parts:' + JSON.stringify(ctx.parts) + '</pre>');
 	ctx.resp.end();
 });
 
