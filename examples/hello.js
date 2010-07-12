05736fecb0a43a9b7e37fa76de64928ece7f101a
Kadir Pekel <kadirpekel@gmail.com>
2010-07-12 13:09:12 +0000

    path variables implemented and jandler context object compacted

--- examples/hello.js
+++ examples/hello.js
@@ -4,9 +4,10 @@ var sys = require('sys'),
 	http = require('http'),
 	meryl = require('meryl');
 
-meryl.h('GET /hello_world', function(ctx) {
+meryl.h('GET /post/<postid>.html', function(ctx) {
 	ctx.resp.writeHead(200, {'Content-Type': 'text/html'});
 	ctx.resp.write('<h1>Hello, World!</h1>');
+	ctx.resp.write('<pre> url parts:' + JSON.stringify(ctx.parts) + '</pre>');
 	ctx.resp.end();
 });
 
