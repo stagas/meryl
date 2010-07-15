590b5613a702069cc0c4036f802b5922f40ec0c0
Kadir Pekel <kadirpekel@gmail.com>
2010-07-14 21:49:45 -0400

    code reformatted

--- examples/hello.js
+++ examples/hello.js
@@ -4,8 +4,10 @@ var sys = require('sys'),
 	http = require('http'),
 	meryl = require('meryl');
 
-meryl.h('GET /post/{postid}/comment/{commentid}.html', function(req, resp) {
-  resp.writeHead(200, {'Content-Type': 'text/html'});
+meryl.h('GET /post/{postid}/comment/{commentid}.html', function (req, resp) {
+	resp.writeHead(200, {
+		'Content-Type': 'text/html'
+	});
 	resp.write("<h1>You are reading post #" + req.params.postid + "</h1>");
 	resp.write("<h2>You are reading comment #" + req.params.commentid + "</h2>");
 	resp.end();
