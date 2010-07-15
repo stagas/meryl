ce10842520d3fd4de0f32eec1e5a1ea9d9a6fc47
Kadir Pekel <kadirpekel@gmail.com>
2010-07-16 00:29:42 +0300

    improved infrastructe and added plugin system

--- examples/hello.js
+++ examples/hello.js
@@ -4,11 +4,16 @@ var sys = require('sys'),
 	http = require('http'),
 	meryl = require('meryl');
 
-meryl.h('GET /post/<postid>.html', function(ctx) {
-	ctx.resp.writeHead(200, {'Content-Type': 'text/html'});
-	ctx.resp.write('<h1>Hello, World!</h1>');
-	ctx.resp.write('<pre> url parts:' + JSON.stringify(ctx.parts) + '</pre>');
-	ctx.resp.end();
+meryl.p('throw-error', function() {
+	throw new Exception('oops!');
+});
+
+meryl.p('static-header', function() {
+	this.headers.Server = 'Node Http Server';
+});
+
+meryl.h('GET /post/<postid>.html', function() {
+	return "<p>Hello, World! You're reading the post #" + this.postid +"</p>";
 });
 
 http.createServer(meryl.cgi).listen(8080);
