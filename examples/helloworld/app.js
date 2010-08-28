cf74cb521763b54867659a0063fbb8687b380485
Kadir Pekel <kadirpekel@gmail.com>
2010-08-28 18:39:57 -0400

    added proper exception handling with http statuses and demonstrated in hello world example

--- examples/helloworld/app.js
+++ examples/helloworld/app.js
@@ -5,9 +5,23 @@ meryl.p('GET <whatever>', function(req, resp, chain) {
 	chain();
 });
 
+meryl.p('POST .*', function() {
+	this.status = 405;
+	throw new Error('method not allowed');
+});
+
+meryl.p('{method} /private/.*', function() {
+	this.status = 404;
+	throw new Error('access denied');
+});
+
 meryl.h('GET /', function (req, resp) {
 	this.send("<h1>Hello, World</h1>");
 });
 
-require('http').createServer(meryl.cgi()).listen(3000);
+meryl.h('GET /exception', function (req, resp) {
+	this.send(1);
+});
+
+require('http').createServer(meryl.cgi({prod:true})).listen(3000);
 require('sys').debug('serving http://localhost:3000');
