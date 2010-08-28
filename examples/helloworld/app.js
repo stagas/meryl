e0d9113b313e259335d51d139153845fafdcd48c
Kadir Pekel <kadirpekel@gmail.com>
2010-08-28 16:47:35 -0400

    middleware infrastructre refactored to be pure event based

--- examples/helloworld/app.js
+++ examples/helloworld/app.js
@@ -1,13 +1,13 @@
 var meryl = require('./../../lib/meryl');
 
-meryl.p('GET <whatever>', function() {
+meryl.p('GET <whatever>', function(req, resp, chain) {
 	this.headers['Server'] = 'node';
-	return true;
+	chain();
 });
 
 meryl.h('GET /', function (req, resp) {
-	return "<h1>Hello, World</h1>";
+	this.send("<h1>Hello, World</h1>");
 });
 
-require('http').createServer(meryl.cgi).listen(3000);
+require('http').createServer(meryl.cgi()).listen(3000);
 require('sys').debug('serving http://localhost:3000');
